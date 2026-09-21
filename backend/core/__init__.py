a43d54db2cc6c70776bb563376ec9cd2f1a465f1
Xyr <py658112@gmail.com>
2026-09-21 16:29:59 +0530

    Add pyrogram_manager to core module exports

--- backend/core/__init__.py
+++ backend/core/__init__.py
@@ -1,10 +1,12 @@
 from backend.core.events import event_manager
+from backend.core.pyrogram_client import pyrogram_manager
 from backend.core.sessions import session_manager
 from backend.core.telegram import telegram_manager
 from backend.core.websocket_manager import websocket_manager
 
 __all__ = [
     "event_manager",
+    "pyrogram_manager",
     "session_manager",
     "telegram_manager",
     "websocket_manager",
